9825596fda7202d4010fdad634071d6853ceef08
naveenmudrakola <naveen.mudrakola@gmail.com>
2023-09-06 22:25:43 +0800

    included multi threading

--- sph/Checker.py
+++ sph/Checker.py
@@ -3,17 +3,24 @@ import time
 import requests
 from datetime import datetime, timedelta
 from flask import Flask, jsonify
+import threading
+from multiprocessing import Process
 
 app = Flask(__name__)
 statuses = []
 
-def read_csv(file_name="urls.csv"):
-    with open(file_name, "r") as file:
-        reader = csv.DictReader(file)
-        return list(reader)
 
-def check_urls(urls):
+        
+def check_urls():
+    urls = []
+    with open("urls.csv", "r") as file:
+        reader = csv.DictReader(file)
+        urls = list(reader)
+        print(urls)
     global statuses
+ 
+    print(urls)
+    print("in check_urls")
     while True:
         for url_data in urls:
             try:
@@ -28,15 +35,20 @@ def check_urls(urls):
                 "status_code": status_code,
                 "timestamp": datetime.now()
             })
+            print(statuses)
         time.sleep(600) # 10 minutes
 
 @app.route('/status', methods=['GET'])
 def get_status():
+    print("hello")
     global statuses
     one_hour_ago = datetime.now() - timedelta(hours=1)
     recent_statuses = [status for status in statuses if status["timestamp"] >= one_hour_ago]
     return jsonify(recent_statuses)
 
 if __name__ == "__main__":
-    urls = read_csv()
+    threading.Thread(target=check_urls).start()
     app.run(debug=True, port=5000, threaded=True, use_reloader=False)
+
+    
+    
